37673117d5d144e7c93d8930eb96132b56f81cdb
Latifi Charanek <111158217+fifizika@users.noreply.github.com>
2023-05-29 22:45:14 -0300

    att
    Atualizacoes

--- resources/views/Welcome.blade.php
+++ resources/views/Welcome.blade.php
@@ -9,8 +9,8 @@
       <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
       <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
     </div>
-  <div class="carousel-inner ">
-    <div class="carousel-item active">
+  <div class="carousel-inner mx-auto">
+    <div class="carousel-item active bg-dark">
       <img  class="d-block w-90 mx-auto" src="https://images.unsplash.com/photo-1516924962500-2b4b3b99ea02?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="...">
     </div>
     <div class="carousel-item">
@@ -31,7 +31,6 @@
 </div>
 
 <!--Titulo para produto e a linha horizontal para separar-->
-<br>
 <div class="AlertProduto">
 <hr style="color: black;">
 <h2 class="text-center">PRODUTOS</h2>
@@ -40,12 +39,13 @@
 <br><br><br><br><br><br>
 
 <!--Card dos produtos-->
-<div class="row">
+<div class="row d-flex justify-content-center">
   <!--foreach para puxar o conteudo da pasta app\models e após isso ::all para pegar todo o conteudo da pasta produto, o take(5) para pegar 5 produtos e o "as $produto" para pegar de produto-->
-  @foreach(\App\Models\Produto::all()->take(8) as $produto)
-  <div class="card m-3 " style="width: 18rem;">
+  @foreach(\App\Models\Produto::all()->take(4) as $produto)
+  <div class="col-3 col-md-auto ">
+  <div class="card" style="width: 15rem; height: 30rem;">
   <!-- if onde ele vai contar para ver se em $produto->ProdutoImagem há mais de 0 elementos adicionados, se sim ele irá puxar do banco a URL e colocr a imagem no card, e se nao houver, colocará uma imagem genérica-->
-    @if(count($produto->ProdutoImagem) > 0)
+    @if(count($produto->ProdutoImagem) > 0)3
       <img class="card-img-top w-auto" src="{{$produto->ProdutoImagem[0]->IMAGEM_URL}}">
     @else
       <img class="card-img-top w-auto" src="">
@@ -54,10 +54,12 @@
     <h5 class="card-title text-black">{{$produto->PRODUTO_NOME}}</h5>
     <h5 class="card-title text-black">{{$produto->PRODUTO_PRECO}}</h5>
     <a class="menu__item" href="{{route('produto.show', $produto->PRODUTO_ID)}}">Saiba mais</a>
+</div>
 
   </div>
 </div>
 
 <!--finaliza o foreach-->
 @endforeach
+
 @endsection
